c8a6aee3804f98b5ee01374838aba3fb9ad24347
huynh <huyhcreative@gmail.com>
2026-04-05 20:49:25 +0700

    fix(pwa): set correct icon paths in manifest for nextjs App Router

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,14 +11,28 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#caa052',
     icons: [
       {
-        src: '/icon',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: '/icon?size=192',
+        src: '/icon/192',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon/512',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon/192',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/icon/512',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
       }
     ],
   };
