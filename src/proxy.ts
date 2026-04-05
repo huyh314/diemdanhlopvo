25a59d2e6994eb6d8887dd53e3eec7e3f0bc15c8
huynh <huyhcreative@gmail.com>
2026-04-05 21:27:05 +0700

    fix: exclude PWA resources from middleware auth interception

--- src/proxy.ts
+++ src/proxy.ts
@@ -12,9 +12,11 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
-         * - js (custom static scripts)
+         * - manifest.webmanifest, manifest.json (PWA manifests)
+         * - sw.js (Service Worker)
+         * - icon (Dynamic icons for PWA)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|js|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|manifest.webmanifest|manifest.json|sw.js|icon/.*|.*\\.(?:svg|png|jpg|jpeg|gif|webp|js|ts)$).*)',
     ],
 }
